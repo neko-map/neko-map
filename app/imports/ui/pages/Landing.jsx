84e5d6d51b4096ea0331516d172946e8c28573fb
nathancorpuz <77780824+nathancorpuz@users.noreply.github.com>
2021-04-13 21:47:50 -1000

    landingpage version 1

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,18 +5,44 @@ import { Grid, Image } from 'semantic-ui-react';
 class Landing extends React.Component {
   render() {
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
-      </Grid>
+      <div className='landing-page'>
+        <Grid verticalAlign='middle' textAlign='center'>
+          <Grid.Row className="landing-page-header-background">
+            <Grid.Column>
+              <b className='landing-page-title'>NEKO MAP</b>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <b>@UHMANOA</b>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Grid verticalAlign='middle' textAlign='center' className='landing-page'>
+          <Grid.Row columns='equal' className='landing-page-steps'>
+            <Grid.Column>
+              <a href='#/userprofiles' className='white'>1. Create a Profile</a>
+            </Grid.Column>
+            <Grid.Column>
+              <a href='#/catprofiles' className='white'>2. View/Add any cats found in UH Manoa</a>
+            </Grid.Column>
+            <Grid.Column>
+              <a href='#/volunteer' className='white'>3. Volunteer to feed</a>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns='equal'>
+            <Grid.Column>
+              <Image href='#/userprofiles' src="/images/signin.png" bordered/>
+            </Grid.Column>
+            <Grid.Column>
+              <Image href='#/catprofiles' src="/images/cats.png" bordered/>
+            </Grid.Column>
+            <Grid.Column>
+              <Image href='#/volunteer' src="/images/volunteer.png" bordered/>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
     );
   }
 }
