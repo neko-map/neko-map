48f003ea2865d52be1ef8b11fcde38d1c7eae69a
micahima <55009261+micahima@users.noreply.github.com>
2021-04-20 11:17:57 -1000

    Update App.jsx

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -25,10 +25,7 @@ class App extends React.Component {
             <Route path="/signin" component={Signin}/>
             <Route path="/signup" component={Signup}/>
             <Route path="/signout" component={Signout}/>
-            <ProtectedRoute path="/list" component={ListStuff}/>
             <ProtectedRoute path="/volunteer" component={VolunteerForm}/>
-            <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
-            <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
             <Route component={NotFound}/>
           </Switch>
           <Footer/>
