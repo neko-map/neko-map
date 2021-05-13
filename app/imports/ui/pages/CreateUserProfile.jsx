188165a58113066b1e813c07663d984662133e50
Thaneluna <thanel@hawaii.edu>
2021-05-12 21:01:45 -1000

    Update CreateUserProfile.jsx

--- app/imports/ui/pages/CreateUserProfile.jsx
+++ app/imports/ui/pages/CreateUserProfile.jsx
@@ -17,7 +17,6 @@ const formSchema = new SimpleSchema({
   image: {
     type: 'String',
     defaultValue: 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png',
-    optional: true,
   },
 });
 
